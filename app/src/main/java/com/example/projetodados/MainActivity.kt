782a5afcbb4dc2fb9785ee9b72ba42ea829838b2
Antonio1H <antoniohenriquef101@gmail.com>
2024-09-05 16:57:15 -0300

    Inicio do projeto

--- app/src/main/java/com/example/projetodados/MainActivity.kt
+++ app/src/main/java/com/example/projetodados/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ProjetoDadosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "AH",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
